b26890eaeaad414708289a0fa8ae6b5ffbf520d9
FnvaAcademy <67489226+FnvaAcademy@users.noreply.github.com>
2021-09-19 16:36:25 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -1,10 +1,13 @@
-function navigationBar(){
-    let button = $(".navigation-button");
-    let wrapper = $(".navigation-wrapper");
-    
-    button.click(function(){
-        wrapper.toggle();
-    });
+function styling(){
+
+    let window_width = window.outerWidth;
+    let window_height = window.outerHeight;
+
+    if(window_height <= 568){
+        let welcome_caption = document.getElementsByClassName("welcome-caption");
+        welcome_caption[0].style.paddingTop = "40px";
+    }
+   
 }
 
-navigationBar();
+styling();
