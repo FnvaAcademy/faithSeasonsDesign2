8676a785385228ed94e13a85342add3715e46c94
FnvaAcademy <67489226+FnvaAcademy@users.noreply.github.com>
2022-01-24 05:25:39 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -1,13 +1,34 @@
+
+
+function navigationButton(){
+    $('.navigation-button').click(function(){
+        $(".navigation-list-outer-wrapper").slideToggle(400);
+    });
+}
+
+function hideMenu(){
+    $(".about-link").click(function(){
+        $(".navigation-list-outer-wrapper").hide();
+    })
+}
+
 function styling(){
 
     let window_width = window.outerWidth;
     let window_height = window.outerHeight;
 
     if(window_height <= 568){
-        let welcome_caption = document.getElementsByClassName("welcome-caption");
-        welcome_caption[0].style.paddingTop = "40px";
+        let section = document.getElementsByClassName("background-img-wrapper");
+        let mask = document.getElementsByClassName("bg-image-mask");
+        mask[0].style.height = "290vh";
+        section[0].style.height ="290vh";
+        //welcome_caption[0].style.fontSize = "14pt";
     }
    
 }
 
+
+
+navigationButton();
 styling();
+//cardButtons();
